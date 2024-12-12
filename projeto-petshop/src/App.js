659faa3acfb46b684ae1140242c9cc6575c839e5
annalara123 <112590453+annalara123@users.noreply.github.com>
2024-12-12 10:06:26 -0300

    Adicionando Pasta service e o resto dos arquivos

--- projeto-petshop/src/App.js
+++ projeto-petshop/src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import HomePage from "./pages/home/HomePage";
+import AboutUs from "./pages/AboutUs/AboutUsPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MyNavBar from "./components/NavBar/MyNavBar";
+import Scheduling from "./pages/Scheduling/SchedulingPage";
+import StorePage from "./pages/Store/StorePage";
+import AuthPage from "./pages/AuthPage/AuthPage";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+function App() {
+  return (
+    <Router>
+      <div>
+        <MyNavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/loja" element={<StorePage />} />
+          <Route path="/sobre" element={<AboutUs />} />
+          <Route path="/agendamento" element={<Scheduling />} />
+        </Routes>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
